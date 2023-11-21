f0341b704c4ac33595133029e73f0485de6aa9c3
zachcp <zachcp@users.noreply.github.com>
2023-11-21 07:49:45 -0500

    Update to Tauri 2.0 alpha (#6)
    * move to Tuari 2.0plugin system.
    * update deps and conf/TOML
    * fix file JS loading and file handling in the plugin system
    ---------
    
    Co-authored-by: Zachary Charlop-Powers <zcpowers@Zacharys-MacBook-Air.local>

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,24 @@
+
+mod sequences;
+use sequences::fastx::{convert_fastq_to_fasta_tauri, get_seqstats, get_stats};
+use sequences::patterns::check_restriction_sites;
+use sequences::rnapkin::rnapkin_fn;
+use sequences::dna_utils::translate_dna;
+
+  
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_window::init())
+        .plugin(tauri_plugin_dialog::init())
+        .invoke_handler(tauri::generate_handler![
+            get_stats,
+            get_seqstats,
+            rnapkin_fn,
+            check_restriction_sites,
+            convert_fastq_to_fasta_tauri,
+            translate_dna
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,31 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use serde::{Deserialize, Serialize};
-
-mod sequences;
-use sequences::fastx::{convert_fastq_to_fasta_tauri, get_seqstats, get_stats};
-use sequences::patterns::check_restriction_sites;
-use sequences::rnapkin::rnapkin_fn;
-use sequences::dna_utils::translate_dna;
-
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![
-            get_stats,
-            get_seqstats,
-            rnapkin_fn,
-            check_restriction_sites,
-            convert_fastq_to_fasta_tauri,
-            translate_dna
-        ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
-}
+    bioinformaticstoolkit_lib::run();
+  }
 
-#[cfg(test)]
-mod tests {
-    // Note this useful idiom: importing names from outer (for mod tests) scope.
-    // use super::*;
-}
