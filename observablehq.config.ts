0cf49a17fa8c61a3ba95ffe965b2be691d915fe9
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2024-10-15 10:50:12 -0400

    update config.ts

--- observablehq.config.ts
+++ observablehq.config.ts
@@ -43,9 +43,15 @@ export default {
   // Some additional configuration options and their defaults:
   theme: "default", // try "light", "dark", "slate", etc.
   // header: "", // what to show in the header (HTML)
+  // head: ({
+  //   path,
+  // }) => `<link rel="canonical" href="${path.replace(/\/index$/, "/")}">
+  //   <link rel="apple-touch-icon" href="/logo.png">
+  //   <link rel="icon" type="image/png" href="/logo.png" sizes="32x32">`,
+
   // footer: "Built with Observable.", // what to show in the footer (HTML)
   toc: true, // whether to show the table of contents
   // pager: true, // whether to show previous & next links in the footer
-  // root: "docs", // path to the source root for preview
   // output: "dist", // path to the output root for build
+  dynamicPaths: ["./icon.png"],
 };
